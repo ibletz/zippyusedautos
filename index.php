b443db1b34314da74871148514302285d1efefa1
ibletz <ibletz@gmail.com>
2022-03-21 23:46:52 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+session_start();
+
+$db = new PDO('mysql:dbname=zippyusedautos;host=localhost', 'root');
+
+$itemsQuery = $db->prepare("
+    SELECT year, model, price, type_id, class_id, make_id
+    FROM vehicles
+");
+$itemsQuery->execute();
+
+$items = $itemsQuery->rowcount() ? $itemsQuery : [];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zippy's Used Autos</title>
+</head>
+
+<body>
+    <h1>Welcome to Zippy's Used Auto Sales</h1>
+    <div class="list">
+        <?php if(!empty($items)): ?>
+        <ul class="items">
+            <?php foreach($items as $item): ?>
+                <li>
+                    <span class="item"><?php echo $item['year'], ' ';
+                                            echo $item['make_id'], ' ';
+                                            echo $item['model'], ' ';
+                                            echo $item['price'];
+                                        ?> </span>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <span class="sort-order">
+            <label class="sortby">Sort by: </label>
+            <input type="textbox">
+        </span>
+        <?php else: ?>
+            <p>No cars are available.</p>
+        <?php endif; ?>
+    </div>
+</body>
+</html>
